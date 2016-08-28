b8782cee98a68b2d0782843c2c31a8f1f1b157a3
Markus Kobler <markus.kobler@digital.hmrc.gov.uk>
2016-08-28 19:26:50 +0100

    Updating lua

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 #
-# Download and build lua
+# Download and build lua lib
 set -euo pipefail
 
 version=5.3.2
 sha1=7a47adef554fdca7d0c5536148de34579134a973
 
-src=$PWD/lau
-out=$PWD/lua-build
+src=$PWD/liblua
+out=$PWD/out
 
 mkdir -p ${src}
 curl -OL http://www.lua.org/ftp/lua-${version}.tar.gz
 echo "${sha1}  lua-${version}.tar.gz" | sha1sum -c
 tar -C ${src} --strip-components 1 -xf lua-${version}.tar.gz
+
 
 cd ${src}
 make linux
